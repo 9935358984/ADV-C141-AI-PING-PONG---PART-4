12caf1c227f4f874907fab15975efc97d1e2287b
9935358984 <79409322+9935358984@users.noreply.github.com>
2022-03-14 09:01:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,30 @@
-function setup()
-{
-    var canvas =  createCanvas(600,500);
-    canvas.position(270,120);
-    video=createCapture(VIDEO);
-    video.position(50, 50);
-    video.size(180, 200);
-    poseNet=ml5.poseNet(video,modelLoaded);
-    poseNet.on('pose',gotPoses);
-    cannon_one= loadImage('cannon1.png');
-}
+function setup(){
+    var canvas =  createCanvas(700,600);
+    canvas.parent('canvas');
+    
+    video = createCapture(VIDEO);
+    video.size(700, 600);
+    
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+    video.hide();
+    }
 function gotPoses(results)
-  {
-	if(results.length > 0)
-	{
-	  console.log(results);
-	  noseX = results[0].pose.nose.x;
-	  noseY = results[0].pose.nose.y;
-	}
-  }  
-function draw() {
-	game();
-}
-
-function modelLoaded()
 {
-    console.log("Model Loaded");
+  if(results.length > 0)
+  {
+
+    rightWristY = results[0].pose.rightWrist.y;
+    rightWristX = results[0].pose.rightWrist.x;
+    scoreRightWrist =  results[0].pose.keypoints[10].score;
+    console.log(scoreRightWrist);
+  }
 }
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+  }
+  function draw(){
+
+    background(0); 
+    image(video, 0, 0, 700, 600);
+  }
